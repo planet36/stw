6bdf77544d95984f6e06629db27966254927848a
planet36 <planet36@users.noreply.github.com>
2022-08-31 19:43:52 -0500

    Append to FLAGS variables instead of overwriting them

--- config.mk
+++ config.mk
@@ -14,6 +14,6 @@ INCS = `pkg-config --cflags fontconfig`
 LIBS = `pkg-config --libs   xft xrender`
 
 # flags
-CPPFLAGS = -DVERSION=\"$(VERSION)\" -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_POSIX_C_SOURCE=2 $(INCS)
-CFLAGS   = -std=c99 -pedantic -Wall -Wno-shadow -Wno-sign-compare $(CPPFLAGS) $(CPPFLAGS) $(CFLAGS)
-LDFLAGS  = $(LIBS) $(LDFLAGS)
+CPPFLAGS += -DVERSION=\"$(VERSION)\" -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_POSIX_C_SOURCE=2 $(INCS)
+CFLAGS += -std=c99 -pedantic -Wall -Wno-shadow -Wno-sign-compare $(CPPFLAGS)
+LDFLAGS += $(LIBS)
